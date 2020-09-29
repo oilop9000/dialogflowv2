260c5af43564ccac325d1dde2334b0b71a5f3ebf
Cleiton Jorge <cleitonjorge@MacBook-Pro-de-Cleiton.local>
2020-09-29 20:43:21 -0300

    adicionar variavei de configuraçãi no payload

--- dialogflowv2.js
+++ dialogflowv2.js
@@ -17,9 +17,9 @@ module.exports = function(RED) {
 
     this.on('input', function (msg) {
       var dialogFlowNode = RED.nodes.getNode(node.dialogflow);
-      var language = utils.extractValue('string', 'language', node, msg, false);
-      var queryParamsOK = utils.extractValue('boolean', 'queryParamsOK', node, msg, false);
-      var debug = utils.extractValue('boolean', 'debug', node, msg, false);
+      var language = utils.extractValue('string', 'language', node, msg, true);
+      var queryParamsOK = utils.extractValue('boolean', 'queryParamsOK', node, msg, true);
+      var debug = utils.extractValue('boolean', 'debug', node, msg, true);
 
       // exit if empty credentials
       if (dialogFlowNode == null || dialogFlowNode.credentials == null) {
